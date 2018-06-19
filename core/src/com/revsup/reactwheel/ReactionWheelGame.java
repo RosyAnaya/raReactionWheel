9eabee381abd70c3ce049beeb8186c3f6ffc4b38
RevsUp <revsup@lab-311-013.cs.csubak.edu>
2018-06-19 15:39:47 -0700

    first commit

---
+++ core/src/com/revsup/reactwheel/ReactionWheelGame.java
@@ -0,0 +1,34 @@
+package com.revsup.reactwheel;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.revsup.reactwheel.objects.Wheel;
+
+public class ReactionWheelGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Wheel w;
+
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		w = new Wheel(batch);
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0/255f, 121/255f, 107/255f, 1f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		w.render();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		w.dispose();
+
+	}
+}

6145c1dbacd723f6866c09d0f5241062f0b7da0a
RevsUp <revsup@lab-311-013.cs.csubak.edu>
2018-06-20 15:27:14 -0700

    update 6/20/18

--- core/src/com/revsup/reactwheel/objects/Wheel.java
+++ core/src/com/revsup/reactwheel/objects/Wheel.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
@@ -20,17 +21,26 @@ public class Wheel extends ShapeRenderer {
     private float dir;
     private Vector2 target;
     private Vector2 prevTarget;
-    private double dist;
     private float targetRadius;
     private boolean gameRunning;
    private float angleSpeed;
    private boolean targetWasInRange;
+   private Vector2 hitPoint;
+    private float screenWidth = Gdx.graphics.getWidth();
+    private float screenHeight = Gdx.graphics.getHeight();
+    private BitmapFont scoreFont;
+    private BitmapFont highScoreFont;
+    private int score;
+
+
 
     //colors
     private static final Color BACKGROUND = new Color(0/255f, 121/255f, 107/255f, 1f);
     private static final Color RED = new Color(231/255f, 76/255f, 60/255f, 1f);
     private static final Color BLUE= new Color(52/255f, 152/255f, 219/255f, 1f);
-    private static final Color WHITE = new Color(69/255f,90/255f,100/255f,1f);
+    private static final Color GREY = new Color(69/255f,90/255f,100/255f,1f);
+    private static final Color WHITE = new Color(1f,1f,1f,1f);
+    private static final Color BLACK = new Color(33/255f,33/255f,33/255f,1f);
 
     //animation
     private SpriteBatch batch;
@@ -41,23 +51,32 @@ public class Wheel extends ShapeRenderer {
     private float stateTime;
     private boolean targetHit;
 
+    private Texture tapToPlay;
+    private Texture highScore;
+
+
 
     public Wheel(SpriteBatch batch) {
-        float screenWidth = Gdx.graphics.getWidth();
-        float screenHeight = Gdx.graphics.getHeight();
+
         center = new Vector2(screenWidth/2f, screenHeight-screenHeight/3f);
         radius = screenWidth/2.5f;
 
         arm = new Vector2(center.x+radius/1.1f, center.y);
         angleSpeed = 0.0f;
 
+        hitPoint = new Vector2((arm.x*1.1f)/1.165f, arm.y);
+
+        tapToPlay = new Texture("tapscreentext.png");
+        highScore = new Texture("highscore.png");
+
+
         isClockwise = false;
         dir = 1f;
 
         target = new Vector2(center.x, center.y+radius/1.3f);
-        targetRadius = 45.0f;
+        targetRadius = screenWidth/25f;
 
-        dist = 0.0;
+
 
         this.batch = batch;
         targetHit = false;
@@ -66,6 +85,23 @@ public class Wheel extends ShapeRenderer {
 
         gameRunning = false;
         targetWasInRange = false;
+
+        scoreFont = new BitmapFont();
+        scoreFont.getRegion().getTexture().setFilter(
+                Texture.TextureFilter.Linear,
+                Texture.TextureFilter.Linear);
+        scoreFont.getData().setScale(6f);
+
+
+        highScoreFont = new BitmapFont();
+        highScoreFont.getRegion().getTexture().setFilter(
+                Texture.TextureFilter.Linear,
+                Texture.TextureFilter.Linear);
+        highScoreFont.getData().setScale(6f);
+
+        SavedDataManager.getInstance().load();
+
+        score = 0;
 
         initAnimation();
 
@@ -109,9 +145,16 @@ public class Wheel extends ShapeRenderer {
         update();
         this.begin(ShapeType.Filled);
 
+
+       this.setColor(BLACK);
+        this.circle(center.x, center.y,radius/.98f, 100);
         //outer ring
-        this.setColor(WHITE);
+        this.setColor(GREY);
         this.circle(center.x, center.y,radius, 100);
+
+       this.setColor(BLACK);
+        this.circle(center.x, center.y,radius/1.00001f, 100);
+
 
         this.setColor(BACKGROUND);
         this.circle(center.x, center.y, radius/1.1f, 100);
@@ -120,9 +163,29 @@ public class Wheel extends ShapeRenderer {
         setColor(RED);
         this.rectLine(center, arm, 25f);
 
+
+        this.setColor(BLACK);
+        this.circle(center.x, center.y,radius/1.55f, 100);
+
         //center circle
-        setColor(WHITE);
+        setColor(GREY);
         circle(center.x, center.y, radius/1.6f, 100);
+
+        this.setColor(BLACK);
+        this.circle(center.x, center.y,radius/1.93f, 100);
+
+        setColor(BACKGROUND);
+        circle(center.x, center.y, radius/2.0f, 100);
+
+        this.setColor(BLACK);
+        this.circle(center.x, center.y,radius/2.4f, 100);
+
+        setColor(GREY);
+        circle(center.x, center.y, radius/2.5f, 100);
+
+        //TODO: delte later
+        setColor(WHITE);
+        circle(hitPoint.x, hitPoint.y, 10f, 6);
 
         renderTarget();
         this.end();
@@ -133,25 +196,39 @@ public class Wheel extends ShapeRenderer {
         if(targetHit)
             showExplosion();
 
+        scoreFont.draw(batch, String.valueOf(score), center.x, center.y);
+
+        if(!gameRunning)
+            batch.draw(tapToPlay, (screenWidth - tapToPlay.getWidth())/2, screenHeight/4);
+
+        batch.draw(highScore, 20f, 50f);
+
+        highScoreFont.draw(batch, String.valueOf(SavedDataManager.getInstance().getHighScore()),
+                highScore.getWidth()+50, highScore.getHeight()*2);
+
         batch.end();
     }
 
     public void renderTarget() {
         setColor(BLUE);
-        circle(target.x, target.y, targetRadius, 100);
+        circle(target.x, target.y, targetRadius, 6);
         setColor(WHITE);
-        circle(target.x, target.y, 30f, 100);
+        circle(target.x, target.y, targetRadius/1.5f, 6);
         setColor(RED);
-        circle(target.x, target.y, 20f, 100);
+        circle(target.x, target.y, targetRadius/3.0f, 6);
     }
 
     public void checkInput() {
         boolean touched = Gdx.input.justTouched();
 
+        if(distance(hitPoint, target) > 20.0 && distance(hitPoint, target) < 40.0)
+            targetWasInRange = true;
+
         if(gameRunning){
             // target was successfully hit
             if(touched && targetInRange()) {
                 isClockwise = !isClockwise;
+                targetWasInRange = false;
 
                 Random rnd = new Random();
                 float ang = 0.0f + rnd.nextFloat() * (360f - 0f);
@@ -159,33 +236,56 @@ public class Wheel extends ShapeRenderer {
                 targetHit = true;
                 prevTarget = new Vector2(target.x, target.y);
 
-                target = rotate(target, ang);
+                score++;
+
+                do {
+                    target = rotate(target, ang);
+                }while(distance(target, prevTarget) < 200);
+
             } else if(touched && !targetInRange()){
                 stopGame();
-            } else if(!touched && targetWasInRange && dist > 100)
+            } else if(!touched && targetWasInRange && distance(hitPoint, target) > 100)
                 stopGame();
 
         } else {
             if(touched)
                 startGame();
+            else
+                resetGame();
         }
+
+    }
+
+    public void resetGame(){
+        arm.x = center.x+radius/1.1f;
+        arm.y = center.y;
+        hitPoint.x = arm.x/1.08f;
+        hitPoint.y = arm.y;
+        target.x = center.x;
+        target.y = center.y+radius/1.3f;
+        isClockwise = true;
+        targetWasInRange = false;
+
 
     }
 
     public void startGame(){
         gameRunning = true;
         angleSpeed = 0.045f;
+        score = 0;
 
     }
 
     public void stopGame(){
         gameRunning = false;
         angleSpeed = 0.0f;
+        SavedDataManager.getInstance().setKeyHighScore(score);
+        SavedDataManager.getInstance().save();
     }
 
     private boolean targetInRange() {
-        float hitRange = targetRadius * 2f;
-        boolean inRange = (dist <= hitRange);
+        float hitRange = 30f;
+        boolean inRange = (distance(hitPoint, target) <= hitRange);
         return inRange;
     }
 
@@ -206,21 +306,23 @@ public class Wheel extends ShapeRenderer {
 
     }
 
+    public double distance(Vector2 p1, Vector2 p2){
+        return  Math.sqrt(Math.pow((p2.x - p1.x) , 2) +
+                Math.pow((p2.y - p1.y) , 2));
+    }
+
 
 
     public void update() {
-        if(dist > 80.0 && dist < 90.0)
-            targetWasInRange = true;
+
 
         checkInput();
         dir = (isClockwise) ? -1f : 1f;
 
-
-
         arm = rotate(arm, angleSpeed);
+        hitPoint = rotate(hitPoint, angleSpeed);
 
-        dist = Math.sqrt(Math.pow((target.x - arm.x) , 2) +
-                         Math.pow((target.y - arm.y) , 2));
+
 
 
     }
